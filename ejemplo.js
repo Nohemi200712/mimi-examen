a2d607ec48c67f957fab572fd144720b99b35365
Nohemi200712 <Nohemigeronimochable@gmail.com>
2025-04-08 12:03:19 -0600

    Add files via upload

---
+++ ejemplo.js
@@ -0,0 +1,12 @@
+function saludar()
+{
+    let saludar1=prompt("ingrese el nombre");
+    alert("Hola "+saludar1+ " suerte en tu examen");
+}
+
+function calcularCuadrado ()
+{
+    let numcuadrado=parseFloat(prompt("escribe el numero que desea calcular"));
+    let cuadrado= (numcuadrado*numcuadrado);
+    alert ("El cuadrado del número es "+cuadrado);
+}
